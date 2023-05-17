a872f6d5a113d3b0303013b6d566ad37aa567a84
fathiya <133835378+fathiyaa12@users.noreply.github.com>
2023-05-18 01:08:56 +0700

    mengupload file main

---
+++ database/Main.c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void play_game()
+{
+    char *questions[] = {
+        "Tahun berapa bahasa C ditemukan?\nA. 1970\nB. 1972\nC. 1971\nD. 1975\n",
+        "Siapakah salah satu pendiri GitHub dibawah ini..?\nA. Chris Wanstrath\nB. Linus Torvald\nC. Joko Widodo\nD. Dennis Ritchie\n",
+        "Bahasa C dikembangkan dimana?\nA. Centrum Wiskunde & Informatica\nB. Oracle\nC. Bell Labs\nD. Indonesia\n",
+        "Bahasa C merupakan bahasa tingkat apa?\nA. Rendah\nB. Sangat tinggi\nC. Menengah\nD. Tinggi\n",
+        "Bagaimana penulisan syntax untuk menampilkan teks pada layar? \nA. D\nB. Scanf\nC. Prinft\nD. Printf\n",
+        "Berikut termasuk hewan-hewan mamalia, kecuali?\nA. Kambing\nB. Ular\nC. Kucing\nD. Paus\n",
+        "Apakah nama ilmiah untuk air?\nA. Au\nB. Mg\nC. CO2\nD. CO\n",
+        "Apakah huruf kendaraan bermotor untuk provinsi Aceh?\nA. BL\nB. BB\nC. BK\nD. B\n",
+        "Apakah ibukota negara Laos?\nA. Kuala Lumpur\nB. Manila\nC. Kamboja\nD. Viantine\n",
+        "Hubungan timbal balik antara makhluk hidup dan lingkungannya disebut?\nA. Komunitas\nB. Ekosistem\nC. Populasi\nD. Adaptasi\n"};
+    
